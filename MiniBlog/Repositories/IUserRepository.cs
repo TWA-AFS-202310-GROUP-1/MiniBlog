f6b3d13703da34d42760a22046f84499e408eb00
Wency3 <XWen3@slb.com>
2023-11-07 22:02:14 +0800

    feat: add user Repositorey

---
+++ MiniBlog/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using MiniBlog.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MiniBlog.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetUsers();
+        Task<User> CreateUser(User user);
+        Task<User> UpdateUser(User user);
+        Task<User> DeleteUser(string userName);
+        Task<User> GetByName(string name);
+    }
+}
